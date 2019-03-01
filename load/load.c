25c3ec2129be05d093eb2ef30bc1f5e0976cffbf
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2019-03-01 10:48:35 +0100

    Update load.c

--- load/load.c
+++ load/load.c
@@ -6,7 +6,7 @@ int main()
 {
 char nom_fichier[30];       //="complex.txt";
   struct complex * v=NULL; //={{1.1,1.1},{2.2,2.2},{3.3,3.3},{4.4,4.4}};
-  int n=0;
+  int n=0,i;
   scanf("%s",nom_fichier);
 
   n=load(nom_fichier,&v);
@@ -14,9 +14,9 @@ char nom_fichier[30];       //="complex.txt";
   if(v!=NULL)
   {
     struct complex * e=v+3;
-    for(;v<=e;v++)
+    for(i=0;i<n;i++)
     {
-        printf("%f %f\n",v->real,v->img);
+        printf("%f %f\n",(v+i)->real,(v+i)->img);
     }
     free(v);
   }
